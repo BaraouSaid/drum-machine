97da64a441239a3c485b6016a5d758096c23eec3
Said <slaoualy@yahoo.fr>
2023-08-06 19:01:17 +0200

    styling with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,39 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello there !!</h1>
+    <div className="container mx-auto my-auto">
+      <div className="bg-stone-900 flex justify-around items-center h-3/4 p-10">
+        <div className="h-92 grid grid-cols-3 grid-rows-3 gap-3 items-center justify-center">
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center">
+            A
+          </div>
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center ">
+            Z
+          </div>
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center">
+            E
+          </div>
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center">
+            Q
+          </div>
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center">
+            S
+          </div>
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center">
+            D
+          </div>
+          <div className="w-20 h-20  rounded-md bg-white flex items-center justify-center">
+            W
+          </div>
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center">
+            X
+          </div>
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center">
+            C
+          </div>
+        </div>
+        <div className="h-96">Hey</div>
+      </div>
     </div>
   );
 }
