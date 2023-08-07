0eef1492d899fd68ed8f38ddeee95af138fcf3e4
Said <slaoualy@yahoo.fr>
2023-08-07 20:20:21 +0200

    implemented the playSound function

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,69 @@
 import { useState } from 'react';
 import './App.css';
 
-const heaters = [
-  {
-    id: 0,
-    keyboardKey: 'Q',
-    keyCode: 81,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
-  },
-  {
-    id: 1,
-    keyboardKey: 'W',
-    keyCode: 87,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
-  },
-  {
-    id: 2,
-    keyboardKey: 'E',
-    keyCode: 69,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
-  },
-  {
-    id: 3,
-    keyboardKey: 'A',
-    keyCode: 65,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
-  },
-  {
-    id: 4,
-    keyboardKey: 'S',
-    keyCode: 83,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
-  },
-  {
-    id: 5,
-    keyboardKey: 'D',
-    keyCode: 68,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
-  },
-  {
-    id: 6,
-    keyboardKey: 'Z',
-    keyCode: 90,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
-  },
-  {
-    id: 7,
-    keyboardKey: 'X',
-    keyCode: 88,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
-  },
-  {
-    id: 8,
-    keyboardKey: 'C',
-    keyCode: 67,
-    src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
-  },
-];
-
 function App() {
+  const heaters = [
+    {
+      id: 0,
+      keyboardKey: 'Q',
+      keyCode: 81,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+    },
+    {
+      id: 1,
+      keyboardKey: 'W',
+      keyCode: 87,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+    },
+    {
+      id: 2,
+      keyboardKey: 'E',
+      keyCode: 69,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+    },
+    {
+      id: 3,
+      keyboardKey: 'A',
+      keyCode: 65,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+    },
+    {
+      id: 4,
+      keyboardKey: 'S',
+      keyCode: 83,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+    },
+    {
+      id: 5,
+      keyboardKey: 'D',
+      keyCode: 68,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+    },
+    {
+      id: 6,
+      keyboardKey: 'Z',
+      keyCode: 90,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    },
+    {
+      id: 7,
+      keyboardKey: 'X',
+      keyCode: 88,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+    },
+    {
+      id: 8,
+      keyboardKey: 'C',
+      keyCode: 67,
+      src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    },
+  ];
+  //Definition of the function responsible to play the sound when clicked
+  const playSound = (el) => {
+    const audioKey = document.getElementById(el);
+    audioKey.play();
+  };
+
   return (
     <div
       id="drum-machine"
@@ -68,8 +73,11 @@ function App() {
         {heaters.map((heater) => {
           return (
             <div
+              onClick={() => {
+                playSound(heater.keyboardKey);
+              }}
               key={heater.id}
-              id={heater.keyboardKey}
+              id={heater.src}
               className="drum-pad w-20 h-20 rounded-md bg-white flex items-center justify-center  cursor-pointer shadow-md"
             >
               {heater.keyboardKey}
@@ -98,7 +106,7 @@ function App() {
           SOUND EFFECT
         </p>
         <div>
-          <input max="1" min="0" step="0.01" type="range" value="0.69" />
+          <input max="1" min="0" step="0.01" type="range" />
         </div>
         <div className="flex flex-col items-center justify-center">
           <p>Bank</p>
