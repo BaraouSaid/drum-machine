e82745d768a0fcb760cf5c3f7b8c1ca58fb77667
Said <slaoualy@yahoo.fr>
2023-08-08 04:00:14 +0200

    destructured heater objects to extract properties

--- src/components/Drumpads.jsx
+++ src/components/Drumpads.jsx
@@ -1,3 +1,5 @@
+import { PropTypes } from 'react';
+
 const Drumpads = ({ heaters }) => {
   //Definition of the function responsible to play the sound when clicked
   const playSound = (el) => {
@@ -6,27 +8,29 @@ const Drumpads = ({ heaters }) => {
   };
   return (
     <div className="h-92 grid grid-cols-3 gap-3 items-center justify-center">
-      {heaters.map((heater) => {
+      {heaters.map(({ id, src, keyboardKey }) => {
         return (
           <div
             onClick={() => {
-              playSound(heater.keyboardKey);
+              playSound(keyboardKey);
             }}
-            key={heater.id}
-            id={heater.src}
+            key={id}
+            id={src}
             className="drum-pad w-20 h-20 rounded-md bg-white flex items-center justify-center  cursor-pointer shadow-md"
           >
-            {heater.keyboardKey}
-            <audio
-              src={heater.src}
-              id={heater.keyboardKey}
-              className="clip"
-            ></audio>
+            {keyboardKey}
+            <audio src={src} id={keyboardKey} className="clip"></audio>
           </div>
         );
       })}
     </div>
   );
 };
+
+// Drumpads.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   src: PropTypes.string.isRequired,
+//   keyboardKey: PropTypes.string.isRequired,
+// };
 
 export default Drumpads;
