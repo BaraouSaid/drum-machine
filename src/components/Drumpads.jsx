8c65fc88746c53af57c0192502472d7127699d36
Said <slaoualy@yahoo.fr>
2023-10-19 07:11:17 +0200

    Arrabged the pads mobile first

--- src/components/Drumpads.jsx
+++ src/components/Drumpads.jsx
@@ -1,13 +1,14 @@
 const Drumpads = ({ heaters, playSound }) => {
   return (
-    <div className="grid items-center justify-center grid-cols-3 gap-5 m-4 h-92">
+    <div className="grid items-center justify-center grid-cols-3 gap-8 m-4 h-92">
       {heaters.map(({ id, src, keyboardKey, clipName }) => {
         return (
           <div
             onClick={() => {
               playSound(keyboardKey, clipName);
             }}
-            className="flex items-center justify-center w-20 h-20 bg-white rounded-md shadow-md cursor-pointer drum-pad"
+            // className="flex items-center justify-center w-20 h-20 bg-white rounded-md shadow-md cursor-pointer drum-pad"
+            className="flex items-center justify-center bg-white rounded-md shadow-md cursor-pointer w-14 h-14 drum-pad"
             key={id}
             id={src}
           >
